f3b2342f6b47c7e5c1749fe357063acafbc5fe84
Sandeep <sandeepiit.ism@gmail.com>
2025-05-06 17:16:59 +0530

    chatbot code uploaded

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Document Chatbot - OpenAI key',
+    version= '0.0.1',
+    author= 'Sandeep Chowdhury',
+    author_email= 'sandeepiit.ism@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
